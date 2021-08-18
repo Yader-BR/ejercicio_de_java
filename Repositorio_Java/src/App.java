c51624a444710898053c471a171aa208f75d2134
Yader_BR <yaderrua@gmail.com>
2021-08-17 19:55:00 -0500

    Feat:segundo cambio java

--- Repositorio_Java/src/App.java
+++ Repositorio_Java/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
+public static Scanner teclado; 
+public static Double[] notas ; 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        teclado= new Scanner (System.in);
+        notas= new Double[5];
+        for(int i=0;i<5;i++){
+            System.out.println("Ingrese las notas");
+            notas[i]=teclado.nextDouble(); 
+        }
+        for(int j=0;j<5;j++){
+            System.out.println("Esta es la nota de la posicion"+j+"y su nota es"+notas[j]); 
+        }
+        
+        
     }
 }
